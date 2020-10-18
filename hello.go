892f8f9670fe570eb641bc2ca89b0c1938db6383
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-10-18 13:01:54 +0530

    Adds a simple hello world in go
    Fixes: #1
    
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- hello.go
+++ hello.go
@@ -1,3 +1,11 @@
 package main
 
+import "fmt"
 
+func hello() string {
+	return "Hello World, learn Go the hard way"
+}
+
+func main() {
+	fmt.Println(hello())
+}
--- hello_test.go
+++ hello_test.go
@@ -1,1 +1,12 @@
 package main
+
+import "testing"
+
+func TestHelloWorld(t *testing.T) {
+	expected := "Hello World, learn Go the hard way"
+
+	actual := hello()
+	if expected != actual {
+		t.Fatalf("expected %v got %v", expected, actual)
+	}
+}
